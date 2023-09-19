dc6be3947423c9e544e17806496d8c56cd42faa4
bigmanonyi <bigmanonyi4@gmail.com>
2023-09-19 07:01:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Printing a line to the standard error
  *
- * Reurn: 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
