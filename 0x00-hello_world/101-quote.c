c2821f7ef0417bacc798e6202aba15dece6873fe
bigmanonyi <bigmanonyi4@gmail.com>
2023-09-19 06:43:15 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Printing a line to the standard error
+ *
+ * Reurn: 1 (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
