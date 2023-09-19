b9cd3be3623c234658876a648ca2930caad69ade
bigmanonyi <bigmanonyi4@gmail.com>
2023-09-19 13:28:24 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	char c;
+
+	b = 'a';
+	c = 'A';
+	while 
+		(b <= 'z')
+		{
+			putchar(b);
+			b++;
+		}
+	while
+		(c <= 'Z')
+		{
+			putchar(c);
+			c++;
+		}
+	return (0);
+}
