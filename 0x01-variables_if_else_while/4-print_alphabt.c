750c1ccce6d4233cf10225b55340d94f4e508797
bigmanonyi <bigmanonyi4@gmail.com>
2023-09-20 02:03:56 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z')
+		{
+			if ((b != 'q' && b != 'e') && b <= 'z')
+				putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
