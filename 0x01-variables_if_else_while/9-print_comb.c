2e3bf4c3ecf764a38ed708192b44c09baa297729
bigmanonyi <bigmanonyi4@gmail.com>
2023-09-20 12:20:59 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		putchar(b + '0');
+			if (b < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	}
+	putchar('\n');
+	return (0);
+}
